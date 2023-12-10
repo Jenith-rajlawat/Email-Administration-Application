2ad2c19edf98a6cd7789815e835c5251aeee3f04
Jenith Rajlawat <jenithrajlawat@gmail.com>
2023-12-10 12:27:27 +0545

    Asking for Department

--- Email.java
+++ Email.java
@@ -1,5 +1,7 @@
 package EmailApplication;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName; //set as private to ensure encapsulation
 	private String lastName;
@@ -13,10 +15,25 @@ public class Email {
 		this.firstName=firstName; //this refers to the class variable not the local one
 		this.lastName=lastName;
 		System.out.println("Email Created: "+this.firstName+" "+ this.lastName);
+		
+		//Call a method asking for the department -returns department
+		this.department=setDepartment();
+		System.out.println("Department: "+this.department);
 	}
 	
 	
 	//Ask for the department
+	private String setDepartment() {
+		System.out.println(" DEPARTMENT CODES\n1) For Sales\n2) For Development\n3) For Accounting\n0) For none\n Enter department code: ");
+		Scanner in = new Scanner(System.in); //in eclipse ctrl+shift+o to import libraries
+		int choice;
+		choice=in.nextInt();
+		if(choice==1) {return "Sales";	}
+		else if(choice==2) {return "Development";} //ctril+alt+downarrow to duplicate selected line
+		else if(choice==3) {return "Account";}
+		else {return "";}
+		
+	}
 	
 	//Generate the random password
 	
