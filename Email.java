231fee3de73b04194a58403e828fac299a23c99d
Jenith Rajlawat <jenithrajlawat@gmail.com>
2023-12-10 12:04:20 +0545

    Creating a model for Email

---
+++ Email.java
@@ -0,0 +1,20 @@
+package EmailApplication;
+
+public class Email {
+	private String fristNname;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailBoxCapacity;
+	private String alternateEmail;
+	
+	//Constructor to receive the first name and last name
+	
+	//Ask for the department
+	
+	//Generate the random password
+	
+	//Set the alternate email
+	
+	
+}
---
+++ EmailApp.java
@@ -0,0 +1,9 @@
+package EmailApplication;
+
+public class EmailApp {
+	
+	public static void main(String[] args) {
+		System.out.println("How are you?");
+	}
+	
+}
